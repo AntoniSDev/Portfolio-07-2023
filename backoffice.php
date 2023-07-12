4222ae5302eb95108f0ee6d0afe786dcb127fd63
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-07-12 14:44:15 +0200

    login

---
+++ backoffice.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+// Vérifier si la session n'est pas active ou si l'utilisateur n'est pas authentifié
+if (!isset($_SESSION["user"])) {
+    // Redirection vers la page de connexion
+    header("Location: login.php");
+    exit();
+}
+
+// Le reste du code de la page backoffice.php ici
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>AntoniSDev</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+<body>
+  <h1>TEST</h1>
+
+  <!-- Bootstrap core JS-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        
+</body>
+</html>
