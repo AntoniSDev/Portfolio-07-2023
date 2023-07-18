074c629c1c2e0c5b03fd996c79def70b3594b15c
AntoniSDev <52274078+AntoniSDev@users.noreply.github.com>
2023-07-18 11:05:07 +0200

    backoffice

--- backoffice.php
+++ backoffice.php
@@ -3,38 +3,98 @@ session_start();
 
 // Vérifier si la session n'est pas active ou si l'utilisateur n'est pas authentifié
 if (!isset($_SESSION["user"])) {
-    // Redirection vers la page de connexion
-    header("Location: login.php");
-    exit();
+  // Redirection vers la page de connexion
+  header("Location: login.php");
+  exit();
 }
 
+
+include "connect.php";
+
+
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // Get the form data
+  $project_name = $_POST["project_name"];
+  $project_details = $_POST["project_details"];
+  $project_link = $_POST["project_link"];
+  $project_git = $_POST["project_git"];
+  $project_screen = $_FILES["project_screen"]["name"];
+
+
+  move_uploaded_file($_FILES["project_screen"]["tmp_name"], "assets/img/portfolio/" . $project_screen);
+
+
+  $sql = "INSERT INTO projects (project_name, project_details, project_link, project_git, project_screen) VALUES ('$project_name', '$project_details', '$project_link', '$project_screen')";
+  mysqli_query($conn, $sql);
+
+
+  header("Location: portfolio.php");
+}
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>AntoniSDev</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
-<body>
-  <h1>TEST</h1>
 
+<head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>AntoniSDev</title>
+  <!-- Favicon-->
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <!-- Font Awesome icons (free version)-->
+  <script src="https://usefontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+  <!-- Google fonts-->
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/styles.css" rel="stylesheet" />
+</head>
+
+<body>
+
+
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+
+      <form action="add_project.php" method="post" enctype="multipart/form-data">
+        <div class="form-group">
+          <label for="project_name">Nom du projet</label>
+          <input type="text" class="form-control" id="project_name" name="project_name" required>
+        </div>
+        <div class="form-group">
+          <label for="project_details">Détails du projet</label>
+          <textarea class="form-control" id="project_details" name="project_details" rows="3" required></textarea>
+        </div>
+        <div class="form-group">
+          <label for="project_link">Lien du projet</label>
+          <input type="text" class="form-control" id="project_link" name="project_link" required>
+        </div>
+        <div class="form-group">
+          <label for="project_git">GitHub du projet</label>
+          <input type="text" class="form-control" id="project_git" name="project_git" required>
+        </div>
+        <div class="form-group">
+          <label for="project_screen">Screenshot du projet</label>
+          <input type="file" class="form-control-file" id="project_screen" name="project_screen" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Ajouter le projet</button>
+      </form>
+    </div>
+  </div>
+
+  <style>
+    .form-group {
+      margin-bottom: 2rem;
+    }
+  </style>
   <!-- Bootstrap core JS-->
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
-        
+  <!-- Core theme JS-->
+  <script src="js/scripts.js"></script>
 </body>
+
 </html>
--- login.php
+++ login.php
@@ -1,31 +1,46 @@
 <?php
+
+// Démarrer la session
 session_start();
 
+// Vérifier si le formulaire a été soumis
 if (!empty($_POST)) {
-    if (isset($_POST["login"], $_POST["password"]) && !empty($_POST["login"]) && !empty($_POST["password"])) {
-        require_once("connect.php");
 
-        $sql = "SELECT * FROM user WHERE user_name = :user_name";
-        $query = $db->prepare($sql);
-        $query->bindValue(":user_name", $_POST["login"]);
-        $query->execute();
-        $user = $query->fetch();
+  // Vérifier si les champs "login" et "password" sont renseignés
+  if (isset($_POST["login"], $_POST["password"]) && !empty($_POST["login"]) && !empty($_POST["password"])) {
 
-        if (!$user) {
-            die("L'utilisateur et/ou le mot de passe est incorrect");
-        }
+    // Connecter à la base de données
+    require_once("connect.php");
 
-        if ($_POST["password"] != $user["password"]) {
-            die("L'utilisateur et/ou le mot de passe est incorrect");
-        }
+    // Exécuter la requête SQL
+    $sql = "SELECT * FROM user WHERE user_name = :user_name AND user_password = :password";
+    $query = $db->prepare($sql);
+    $query->bindValue(":user_name", $_POST["login"]);
+    $query->bindValue(":password", $_POST["password"]);
+    $query->execute();
 
-        $_SESSION["user"] = ["user" => $user["user_name"]];
-        var_dump($_SESSION);
+    // Récupérer l'utilisateur
+    $user = $query->fetch();
 
-        // Redirection vers la page backoffice.php
-        header("Location: backoffice.php");
-        exit();
+    // Vérifier si l'utilisateur existe
+    if ($user) {
+
+      // Créer une session
+      $_SESSION["user"] = $user;
+
+      // Rediriger vers la page de l'espace membre
+      header("Location: backoffice.php");
+      exit();
+    } else {
+
+      // Afficher un message d'erreur
+      echo "L'utilisateur et/ou le mot de passe est incorrect";
     }
+  } else {
+
+    // Afficher un message d'erreur
+    echo "Veuillez remplir tous les champs";
+  }
 }
 ?>
 
@@ -33,45 +48,48 @@ if (!empty($_POST)) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <meta name="description" content="" />
-        <meta name="author" content="" />
-        <title>AntoniSDev</title>
-        <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
-        <!-- Font Awesome icons (free version)-->
-        <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
-        <!-- Google fonts-->
-        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
-        <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
-        <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="css/styles.css" rel="stylesheet" />
-    </head>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>AntoniSDev</title>
+  <!-- Favicon-->
+  <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+  <!-- Font Awesome icons (free version)-->
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+  <!-- Google fonts-->
+  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet" type="text/css" />
+  <link href="https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic" rel="stylesheet" type="text/css" />
+  <!-- Core theme CSS (includes Bootstrap)-->
+  <link href="css/styles.css" rel="stylesheet" />
+</head>
+
 <body>
-    <div class="container mt-5">
-        <div class="row justify-content-center">
-            <div class="col-lg-4">
-                <h2 class="mb-4">Connexion</h2>
-                <form method="post">
-                    <div class="mb-3">
-                        <label for="login" class="form-label">Login</label>
-                        <input type="text" class="form-control" id="login" name="login" placeholder="Entrez votre login">
-                    </div>
-                    <div class="mb-3">
-                        <label for="password" class="form-label">Mot de passe</label>
-                        <input type="password" class="form-control" id="password" name="password" placeholder="Entrez votre mot de passe">
-                    </div>
-                    <button type="submit" class="btn btn-primary">Se connecter</button>
-                </form>
-            </div>
-        </div>
+  <div class="container mt-5">
+    <div class="row justify-content-center">
+      <div class="col-lg-4">
+        <h2 class="mb-4">Connexion</h2>
+        <form method="post">
+          <div class="mb-3">
+            <label for="login" class="form-label">Login</label>
+            <input type="text" class="form-control" id="login" name="login" placeholder="Entrez votre login">
+          </div>
+          <div class="mb-3">
+            <label for="password" class="form-label">Mot de passe</label>
+            <input type="password" class="form-control" id="password" name="password" placeholder="Entrez votre mot de passe">
+          </div>
+          <button type="submit" class="btn btn-primary">Se connecter</button>
+        </form>
+      </div>
     </div>
-    
-    <!-- Bootstrap core JS-->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-        <!-- Core theme JS-->
-        <script src="js/scripts.js"></script>
+  </div>
+
+  <!-- Bootstrap core JS-->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+  <!-- Core theme JS-->
+  <script src="js/scripts.js"></script>
 </body>
+
 </html>
